b4b36f13762014b246a48f56a94f103191accac9
Zeng Yaoxin <167081415+1997yyyy@users.noreply.github.com>
2024-11-26 12:56:15 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include "colorconverter.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    ColorConverter converter;
+    converter.show();
+    return app.exec();
+}
